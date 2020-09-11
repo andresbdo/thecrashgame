ff4f004688de7785e36c52c42328cc32a39a794e
andresbdo <andresblanco383@gmail.com>
2020-09-11 12:45:17 -0300

    Updates

---
+++ precache-manifest.b834b5502ab9cf8c785bdaab510ea598.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd57d8d71ad9f9ead091de873691df8",
+    "url": "/thecrashgame/index.html"
+  },
+  {
+    "revision": "458125ac52bdd32b5a2e",
+    "url": "/thecrashgame/static/css/main.8e98c00d.chunk.css"
+  },
+  {
+    "revision": "f299f39c1f069c97a56d",
+    "url": "/thecrashgame/static/js/2.1bd3c1d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thecrashgame/static/js/2.1bd3c1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "458125ac52bdd32b5a2e",
+    "url": "/thecrashgame/static/js/main.fa42caf0.chunk.js"
+  },
+  {
+    "revision": "417e7b7ff774b314de3c",
+    "url": "/thecrashgame/static/js/runtime-main.6076cd24.js"
+  },
+  {
+    "revision": "be62f60c29e4bf38a7a2fc07507f6be5",
+    "url": "/thecrashgame/static/media/bomb.be62f60c.svg"
+  },
+  {
+    "revision": "1cd76f4d9d36f2f081496f33f8ede5f2",
+    "url": "/thecrashgame/static/media/explosion.1cd76f4d.svg"
+  },
+  {
+    "revision": "8780037d16bf0534024482a3e0996327",
+    "url": "/thecrashgame/static/media/logo.8780037d.svg"
+  },
+  {
+    "revision": "e2d5f8097ffd66d5b38831412a014fb7",
+    "url": "/thecrashgame/static/media/user.e2d5f809.svg"
+  },
+  {
+    "revision": "2d9b6e2d6bec182cfae3bd6c93f1eae6",
+    "url": "/thecrashgame/static/media/wallet.2d9b6e2d.svg"
+  }
+]);
